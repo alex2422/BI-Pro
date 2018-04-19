6e9facc4a37ea0415693f0156edd8fdd915bfeeb
Alexander Martin Kaspersen <alex2422@edu.eal.dk>
2018-04-19 11:12:35 +0200

    Tilføjede Test projekt

---
+++ RevisionFyn.BI-Pro/RevisionFyn.BI-Pro.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RevisionFyn.BI-Pro.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RevisionFyn.BI-Pro.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("09d8ce96-c706-4001-b573-b26a8d992e2f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
