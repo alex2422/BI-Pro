3e19fabaff4df1c683c5681a214b86bebbe29fb5
Alexander Martin Kaspersen <alex2422@edu.eal.dk>
2018-05-01 11:58:32 +0200

    Tilføjede dll til Controller projekt

--- RevisionFyn.BI-Pro/RevisionFyn.BI-Pro.Controller/MainMenuController.cs
+++ RevisionFyn.BI-Pro/RevisionFyn.BI-Pro.Controller/MainMenuController.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 
 namespace RevisionFyn.BI_Pro.Controller
 {
     class MainMenuController
     {
+        #region Variables
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
     }
 }
