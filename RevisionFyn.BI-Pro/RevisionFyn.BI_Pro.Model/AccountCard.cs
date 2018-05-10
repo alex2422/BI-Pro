e2773d8d64c89957188fa8c3b8be4d3ba39330df
Mads Ravn <mads194h@live.dk>
2018-05-10 10:10:04 +0200

    Rettet nogle ting vedrørende accountCard classen

--- RevisionFyn.BI-Pro/RevisionFyn.BI_Pro.Model/AccountCard.cs
+++ RevisionFyn.BI-Pro/RevisionFyn.BI_Pro.Model/AccountCard.cs
@@ -8,12 +8,13 @@ namespace RevisionFyn.BI_Pro.Model
 {
     class AccountCard
     {
-        public int AccountCardID { get; set; }
+        public int CaseID { get; set; }
         public int Year { get; set; }
         public int MainEmployee { get; set; }
         public List<int> otherEmployees;
         public int NumberOfTasks { get; set; }
         public int InvoicePrice { get; set; }
-        public int Coverage { get; set; }
+        public int Balance { get; set; }
+        public int TotalConsumption { get; set; }
     }
 }
