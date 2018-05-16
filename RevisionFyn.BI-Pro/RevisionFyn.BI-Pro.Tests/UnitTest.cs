822b98449bdbaa439939ca5193dee901b3b561de
Alexander Martin Kaspersen <alex2422@edu.eal.dk>
2018-05-16 04:26:13 +0200

    Startet på at implementere statistikberegning og tilhørende unit tests.

---
+++ RevisionFyn.BI-Pro/RevisionFyn.BI-Pro.Tests/UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RevisionFyn.BI_Pro.Model;
+
+namespace RevisionFyn.BI_Pro.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Should_CalculateAverage_When_StraightListProvided()
+        {
+            CustomStatistics customStatistics = new CustomStatistics();
+
+            List<double> listOfData = new List<double>() { 10, 5, 3, 6};
+
+            Assert.AreEqual(6, customStatistics.Average(listOfData));
+        }
+
+        [TestMethod]
+        public void Should_CalculateAverage_When_DecimalListProvided()
+        {
+            CustomStatistics customStatistics = new CustomStatistics();
+
+            List<double> listOfData = new List<double>() { 10.0, 5, 3, 6.0, 4.5 };
+
+            Assert.AreEqual(5.7, customStatistics.Average(listOfData));
+        }
+    }
+}
--- RevisionFyn.BI-Pro/RevisionFyn.BI_Pro.Model/CustomStatistics.cs
+++ RevisionFyn.BI-Pro/RevisionFyn.BI_Pro.Model/CustomStatistics.cs
@@ -12,5 +12,25 @@ namespace RevisionFyn.BI_Pro.Model
         public int ChoosenStatisticsTypeID { get; set; }
         public int ChoosenStatisticsCalculationID { get; set; }
         public List<Company> ChoosenCompanies { get; set; }
+
+        public double Average(List<double> listOfInputData)
+        {
+            return listOfInputData.Average();
+        }
+
+        public double Highest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Lowest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Count()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
