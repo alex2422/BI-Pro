7cf15848f1fa848325e3a27215db33c759655583
Alexander Martin Kaspersen <alex2422@edu.eal.dk>
2018-05-17 19:18:52 +0200

    Tilføjet to do kommentarere

--- RevisionFyn.BI-Pro/RevisionFyn.BI-Pro.Tests/UnitTest.cs
+++ RevisionFyn.BI-Pro/RevisionFyn.BI-Pro.Tests/UnitTest.cs
@@ -8,24 +8,28 @@ namespace RevisionFyn.BI_Pro.Tests
     [TestClass]
     public class UnitTest
     {
+        // TO DO: Test negative listOfData
+        // TO DO: Test long deciamal listOfData
+        // TO DO: Test empty listOfData
+
         [TestMethod]
         public void Should_CalculateAverage_When_StraightListProvided()
         {
-            CustomStatistics customStatistics = new CustomStatistics();
+            //CustomStatistics customStatistics = new CustomStatistics();
 
             List<double> listOfData = new List<double>() { 10, 5, 3, 6};
 
-            Assert.AreEqual(6, customStatistics.Average(listOfData));
+            Assert.AreEqual(6, CustomStatistics.Average(listOfData));
         }
 
         [TestMethod]
         public void Should_CalculateAverage_When_DecimalListProvided()
         {
-            CustomStatistics customStatistics = new CustomStatistics();
+            //CustomStatistics customStatistics = new CustomStatistics();
 
             List<double> listOfData = new List<double>() { 10.0, 5, 3, 6.0, 4.5 };
 
-            Assert.AreEqual(5.7, customStatistics.Average(listOfData));
+            Assert.AreEqual(5.7, CustomStatistics.Average(listOfData));
         }
     }
 }
